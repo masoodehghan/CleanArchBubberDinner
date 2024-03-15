7ebd93305726935efcaa03b23618a6ba60b6703d
masood <masood8204@gmail.com>
2024-03-16 00:02:56 +0330

    add common folder to domain

---
+++ BubberDinner.Domain/Common/Errors/Errors.User.cs
@@ -0,0 +1,14 @@
+using ErrorOr;
+
+namespace BubberDinner.Domain.Common.Errors;
+
+public static partial class Errors
+{
+    public static class User
+    {
+        public static Error DuplicateEmail => Error.Conflict(code: "User.DuplicateEmail",
+                                                             description: "email exist");
+                                                              
+    }
+}
+
